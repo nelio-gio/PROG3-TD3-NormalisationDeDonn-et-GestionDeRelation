62601ff52836130e341e11df41e5b713e400ecfb
Ryan Andriamahery <ryan@hei.school>
2025-12-16 14:44:16 +0300

    chore: improve DBConnection with comments

--- src/main/java/DBConnection.java
+++ src/main/java/DBConnection.java
@@ -4,11 +4,13 @@ import java.sql.SQLException;
 
 public class DBConnection {
 
-    public Connection getConnection () {
+    public Connection getConnection() {
         try {
-            return DriverManager.getConnection(System.getenv("JDBC_URl"), System.getenv("USER"), System.getenv("PASSWORD"));
-        }
-        catch (SQLException e) {
+            String jdbcURl = System.getenv("JDBC_URl"); //jdbc:postgresql://localhost:5432/mini_dish_db
+            String user = System.getenv("USER"); //mini_dish_db_manager
+            String password = System.getenv("PASSWORD"); //123456
+            return DriverManager.getConnection(jdbcURl, user, password);
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
